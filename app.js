0a5057bb1dd9529ac100c52b8e9bf644dcacfea2
Kp Adeyinka <65085595+D3monSlayer64@users.noreply.github.com>
2020-12-12 11:37:34 -0500

    lol

--- app.js
+++
@@ -1,7 +0,0 @@
-const filter = (m) =>
-msg.author.id === m.author.id && m.channel.id === msg.channel.id;
-const collector = msg.channel.createMessageCollector(filter, {
-max: 1,
-time: 120000,
-errors: ["time"],
-});
